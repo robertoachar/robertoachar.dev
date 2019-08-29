d5e289200f0cc87a55e9408d7e19b39858f1b09a
Roberto Achar <robertoachar@gmail.com>
2019-08-29 19:34:27 -0300

    fix: gatsby-source-filesystem path

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
-        path: '${__dirname}/src/images'
+        path: `${__dirname}/src/images`
       }
     },
     'gatsby-transformer-sharp',
