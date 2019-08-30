6f91fbc3b7e3a4c1fbf296a033785b5a74b71520
Roberto Achar <robertoachar@gmail.com>
2019-08-30 18:28:20 -0300

    refactor: improve post

--- src/templates/Post.jsx
+++ src/templates/Post.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
+import Layout from '../components/Layout';
+import SEO from '../components/SEO';
+
 const Post = ({ data }) => {
   const post = data.markdownRemark;
 
   return (
-    <>
-      <h1>{post.frontmatter.title}</h1>
+    <Layout>
+      <SEO title={post.frontmatter.title} />
+      <h2>{post.frontmatter.title}</h2>
       <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
-    </>
+    </Layout>
   );
 };
 
