63569b8dd3ff8c7c016dbff452d597558c802ed1
Roberto Achar <robertoachar@gmail.com>
2019-08-30 18:56:53 -0300

    feature: add pagination

--- src/templates/PostList.jsx
+++ src/templates/PostList.jsx
@@ -4,15 +4,31 @@ import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 import Posts from '../components/Posts';
+import Pagination from '../components/Pagination';
 
-const PostList = ({ data }) => {
+const PostList = ({ data, pageContext }) => {
   const posts = data.allMarkdownRemark.edges;
+
+  const { currentPage, numPages } = pageContext;
+  const isFirst = currentPage === 1;
+  const isLast = currentPage === numPages;
+  const prevPage =
+    currentPage - 1 === 1 ? '/blog' : `/blog/page/${currentPage - 1}`;
+  const nextPage = `/blog/page/${currentPage + 1}`;
 
   return (
     <Layout>
       <SEO title="Blog" />
       <h1>Post List</h1>
       <Posts posts={posts} />
+      <Pagination
+        isFirst={isFirst}
+        isLast={isLast}
+        currentPage={currentPage}
+        numPages={numPages}
+        prevPage={prevPage}
+        nextPage={nextPage}
+      ></Pagination>
     </Layout>
   );
 };
