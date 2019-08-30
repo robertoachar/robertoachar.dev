1ef1630ca03687f30e764b93f0f6859455e5e248
Roberto Achar <robertoachar@gmail.com>
2019-08-30 18:27:35 -0300

    fix: change seo to SEO

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
-import SEO from '../components/seo';
+import SEO from '../components/SEO';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home Page" />
+    <h1>HOME PAGE</h1>
   </Layout>
 );
 
