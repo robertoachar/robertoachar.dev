00be34ba6ca0a9bb764901415d9f324eca7fe641
Roberto Achar <robertoachar@gmail.com>
2019-08-30 17:59:48 -0300

    refactor: improve Avatar

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 import { graphql, useStaticQuery } from 'gatsby';
 import Img from 'gatsby-image';
+
+const Photo = styled(Img)`
+  border-radius: 50%;
+  height: 3.75rem;
+  margin: auto;
+  width: 3.75rem;
+`;
 
 const Avatar = () => {
   const { avatarImage } = useStaticQuery(graphql`
@@ -15,7 +23,7 @@ const Avatar = () => {
     }
   `);
 
-  return <Img fixed={avatarImage.childImageSharp.fixed} />;
+  return <Photo fixed={avatarImage.childImageSharp.fixed} />;
 };
 
 export default Avatar;
