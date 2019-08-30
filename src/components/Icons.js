24e73221ed546c434ed23aed8ff3771393f2f20f
Roberto Achar <robertoachar@gmail.com>
2019-08-30 17:55:35 -0300

    feature: add font-awesome

---
+++ src/components/Icons.js
@@ -0,0 +1,32 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faArrowUp,
+  faDesktop,
+  faHome,
+  faLightbulb,
+  faSearch,
+  faTh
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  faFacebook,
+  faGithub,
+  faLinkedin,
+  faYoutube,
+  faTwitter
+} from '@fortawesome/free-brands-svg-icons';
+
+library.add(
+  /* solid */
+  faArrowUp,
+  faDesktop,
+  faHome,
+  faLightbulb,
+  faSearch,
+  faTh,
+  /* brand */
+  faFacebook,
+  faGithub,
+  faLinkedin,
+  faYoutube,
+  faTwitter
+);
