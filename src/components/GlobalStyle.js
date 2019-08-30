45d07d43a4dbf8e65e7c2c5f5041145fa89fe248
Roberto Achar <robertoachar@gmail.com>
2019-08-30 17:53:37 -0300

    feature: add GlobalStyle

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  html {
+    scroll-behavior: smooth;
+  }
+
+  body {
+    background-color: #ffffff;
+    color: #000000;
+    font-family: sans-serif;
+    font-size: 16px;
+    font-weight: 400;
+    text-align: left;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+  }
+`;
+
+export default GlobalStyle;
