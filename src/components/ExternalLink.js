8f1999a9523a07edf2732e1f91a8c5753126bac6
Roberto Achar <robertoachar@gmail.com>
2019-09-01 10:40:39 -0300

    feature: add ExternalLink

---
+++ src/components/ExternalLink.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const ExternalLink = styled.a`
+  color: #90cdf4;
+  text-decoration: none;
+  transition: color 0.5s;
+
+  &:hover {
+    color: #63b3ed;
+  }
+`;
+
+export default ExternalLink;
