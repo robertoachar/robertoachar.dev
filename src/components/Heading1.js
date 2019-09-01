d8cec2756a289f4a313b92a7e7bc369cef08e82b
Roberto Achar <robertoachar@gmail.com>
2019-09-01 10:40:22 -0300

    feature: add Heading1

---
+++ src/components/Heading1.js
@@ -0,0 +1,14 @@
+import styled, { css } from 'styled-components';
+
+const base = css`
+  border: 0;
+  margin: 0;
+  padding: 0;
+`;
+
+const Heading1 = styled.h1`
+  ${base}
+  font-weight: 600;
+`;
+
+export default Heading1;
