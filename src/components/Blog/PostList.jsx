3fbea19807eea9bd1f92473d44496454b2109411
Roberto Achar <robertoachar@gmail.com>
2019-09-02 12:21:59 -0300

    refactor: add grid to PostContainer

--- src/components/Blog/PostList.jsx
+++ src/components/Blog/PostList.jsx
@@ -4,7 +4,12 @@ import styled from 'styled-components';
 
 import PostItem from './PostItem';
 
-const PostsContainer = styled.div``;
+const PostsContainer = styled.section`
+  display: grid;
+  grid-gap: 2.5rem;
+  grid-template-columns: repeat(auto-fill, minmax(256px, 1fr));
+  padding: 2.5rem;
+`;
 
 const PostList = ({ posts }) => {
   return (
