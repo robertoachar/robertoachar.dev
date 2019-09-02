c944d6b8837d408742b804c187483137876b389d
Roberto Achar <robertoachar@gmail.com>
2019-09-01 23:55:15 -0300

    feature: add About Page

---
+++ src/pages/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+import SEO from '../components/SEO';
+import { Heading1 } from '../components/Common';
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Sobre" />
+    <Heading1>About Page</Heading1>
+  </Layout>
+);
+
+export default IndexPage;
