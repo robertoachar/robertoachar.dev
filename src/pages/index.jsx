e532b1aabbdf7fbc2e285043551d3370790db7d8
Roberto Achar <robertoachar@gmail.com>
2019-09-01 23:55:48 -0300

    refactor: use components instead html

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
+import { Heading1 } from '../components/Common';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home Page" />
-    <h1>HOME PAGE</h1>
+    <Heading1>Home Page</Heading1>
   </Layout>
 );
 
