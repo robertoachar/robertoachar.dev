7732ee3065f7c672bf7d4ce2b8e017335aa80400
Roberto Achar <robertoachar@gmail.com>
2019-09-02 12:24:08 -0300

    refactor: add Cards

--- src/components/Blog/PostItem.jsx
+++ src/components/Blog/PostItem.jsx
@@ -1,76 +1,104 @@
 import React from 'react';
 import t from 'prop-types';
 import styled from 'styled-components';
-import { Link } from 'gatsby';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const PostItemLink = styled(Link)`
-  color: #8899a6;
-  text-decoration: none;
-  transition: color 0.5s;
+import { Hyperlink, Paragraph } from '../Common';
+
+import image01 from '../../../static/01.jpg';
+import image02 from '../../../static/02.jpeg';
+import image03 from '../../../static/03.png';
+
+const Card = styled.div`
+  background-color: var(--sidebar-background);
+  border-radius: ${({ theme }) => theme.radius['radius-200']};
+  box-shadow: ${({ theme }) => theme.shadow['shadow-300']};
+  color: var(--text);
+  overflow: hidden;
+  transition: transform 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
 
   &:hover {
-    color: #1fa1f2;
+    box-shadow: ${({ theme }) => theme.shadow['shadow-400']};
+    transform: translate(0, -2px);
   }
 `;
 
-const PostItemWrapper = styled.section`
-  align-items: center;
-  border-bottom: 1px solid #38444d;
-  display: flex;
-  padding: 2rem 3rem;
-  width: 100%;
+const CardCover = styled.div`
+  background: url(${image01}) no-repeat center center;
+  background-size: cover;
+  height: 192px;
+  position: relative;
 `;
 
-const PostItemTag = styled.div`
-  align-items: center;
-  background: black;
-  border-radius: 50%;
-  color: #fff;
-  display: flex;
-  font-size: 1.3rem;
-  font-weight: 700;
-  justify-content: center;
-  min-height: 90px;
-  min-width: 90px;
+const CardTag = styled.span`
+  background: var(--yellow);
+  bottom: 0;
+  color: var(--sidebar-background);
+  font-size: ${({ theme }) => theme.font.size.small};
+  font-weight: ${({ theme }) => theme.font.weight.bold};
+  left: 0;
+  letter-spacing: 0.05rem;
+  padding: 0.5rem 1rem;
+  position: absolute;
   text-transform: uppercase;
 `;
 
-const PostItemInfo = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-left: 1.5rem;
+const CardBody = styled.div`
+  padding: 1rem 1.5rem 0;
 `;
 
-const PostItemDate = styled.time`
-  font-size: 0.9rem;
-`;
-
-const PostItemTitle = styled.h1`
-  font-size: 1.6rem;
-  font-weight: 700;
-  margin: 0.2rem 0 0.5rem;
-`;
-
-const PostItemDescription = styled.p`
-  font-size: 1.2rem;
-  font-weight: 300;
-  line-height: 1.2;
+const CardTitle = styled.h2`
+  border: 0;
   margin: 0;
+  padding: 0;
+  font-size: ${({ theme }) => theme.font.size.medium};
+  font-weight: ${({ theme }) => theme.font.weight.semibold};
+`;
+
+const CardDescription = styled(Paragraph)`
+  color: var(--text-light);
+  margin-top: 0.5rem;
+`;
+
+const CardFooter = styled.div`
+  color: var(--text-light);
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 1.5rem;
+`;
+
+const CardDate = styled.span`
+  font-size: ${({ theme }) => theme.font.size.small};
+`;
+
+const CardTime = styled.div`
+  display: flex;
+  font-size: ${({ theme }) => theme.font.size.small};
+`;
+
+const CardTimeText = styled.span`
+  margin-left: 0.5rem;
 `;
 
 const PostItem = ({ post, slug, timeToRead }) => (
-  <PostItemLink to={slug}>
-    <PostItemWrapper>
-      <PostItemTag>{post.category}</PostItemTag>
-      <PostItemInfo>
-        <PostItemDate>
-          {`${post.date} - ${timeToRead} minuto(s) de leitura`}
-        </PostItemDate>
-        <PostItemTitle>{post.title}</PostItemTitle>
-        <PostItemDescription>{post.description}</PostItemDescription>
-      </PostItemInfo>
-    </PostItemWrapper>
-  </PostItemLink>
+  <Hyperlink to={slug}>
+    <Card>
+      <CardCover>
+        <CardTag>{post.category}</CardTag>
+      </CardCover>
+      <CardBody>
+        <CardTitle>{post.title}</CardTitle>
+        <CardDescription>{post.description}</CardDescription>
+      </CardBody>
+      <CardFooter>
+        <CardDate>{post.date}</CardDate>
+        <CardTime>
+          <FontAwesomeIcon icon={['far', 'clock']} />
+          <CardTimeText>{`${timeToRead} min de leitura`}</CardTimeText>
+        </CardTime>
+      </CardFooter>
+    </Card>
+  </Hyperlink>
 );
 
 PostItem.propTypes = {
