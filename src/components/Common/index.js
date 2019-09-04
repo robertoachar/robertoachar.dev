7bc10f98084834d1bc7b4c63d43671954910a870
Roberto Achar <robertoachar@gmail.com>
2019-09-04 00:29:27 -0300

    feature: add Heading2

--- src/components/Common/index.js
+++ src/components/Common/index.js
@@ -1,8 +1,9 @@
 import ExternalLink from './ExternalLink';
 import Heading1 from './Heading1';
+import Heading2 from './Heading2';
 import Hyperlink from './Hyperlink';
 import Image from './Image';
 import Paragraph from './Paragraph';
 import Span from './Span';
 
-export { ExternalLink, Heading1, Hyperlink, Image, Paragraph, Span };
+export { ExternalLink, Heading1, Heading2, Hyperlink, Image, Paragraph, Span };
