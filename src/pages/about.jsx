5c71235c7502aae60fea8c7ad4fd3606450f5ad4
Roberto Achar <robertoachar@gmail.com>
2019-09-04 17:57:56 -0300

    feature: add About Page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 
 import SEO from '../components/SEO';
-import PageTitle from '../components/PageTitle';
+import Content from '../components/Content';
+
+import AboutContent from '../../content/pages/about.mdx';
 
 const Container = styled.div`
   margin: 0 auto;
   max-width: 960px;
+  padding: 2rem 1.5rem 1rem;
 `;
 
 const IndexPage = () => (
   <Container>
     <SEO title="Sobre mim" />
-    <PageTitle>Sobre mim</PageTitle>
+    <Content>
+      <AboutContent />
+    </Content>
   </Container>
 );
 
