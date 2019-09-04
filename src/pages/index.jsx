c89e53f9a671d720e573da3d0f40ee3b44321a67
Roberto Achar <robertoachar@gmail.com>
2019-09-04 17:58:09 -0300

    feature: add Home Page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 
 import SEO from '../components/SEO';
-import PageTitle from '../components/PageTitle';
+import Content from '../components/Content';
+
+import HomeContent from '../../content/pages/home.mdx';
 
 const Container = styled.div`
   margin: 0 auto;
   max-width: 960px;
+  padding: 2rem 1.5rem 1rem;
 `;
 
 const IndexPage = () => (
   <Container>
     <SEO title="Home Page" />
-    <PageTitle>Home Page</PageTitle>
+    <Content>
+      <HomeContent />
+    </Content>
   </Container>
 );
 
