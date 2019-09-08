7362ff3f11c3f1d4a616c0bfff8803161010cb8e
Roberto Achar <robertoachar@gmail.com>
2019-09-08 16:44:24 -0300

    refactor: update background color

--- src/components/MDX/InlineCode.js
+++ src/components/MDX/InlineCode.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 
 const Markup = styled.code`
-  background-color: var(--secondary);
+  background-color: var(--background-dark);
   border-radius: ${({ theme }) => theme.radius['radius-100']};
+  font-family: ${({ theme }) => theme.font.family.mono};
   padding: 0.125rem 0.25rem;
 `;
 
