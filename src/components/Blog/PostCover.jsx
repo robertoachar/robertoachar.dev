43c63c2fcdef6a7d803a988acbca5cfa682ae78c
Roberto Achar <robertoachar@gmail.com>
2019-09-09 13:06:39 -0300

    feature: add PostCover

---
+++ src/components/Blog/PostCover.jsx
@@ -0,0 +1,51 @@
+/* eslint react/forbid-prop-types: 0 */
+
+import React from 'react';
+import t from 'prop-types';
+import styled from 'styled-components';
+import Img from 'gatsby-image';
+
+const Figure = styled.figure`
+  margin-top: 1rem;
+  position: relative;
+`;
+
+const Caption = styled.figcaption`
+  background-color: var(--background-dark);
+  bottom: 0;
+  color: var(--secondary);
+  font-size: ${({ theme }) => theme.font.size.extrasmall};
+  font-style: italic;
+  padding: 0.25rem 0.5rem;
+  position: absolute;
+  right: 0;
+`;
+
+const Text = styled.span``;
+
+const Link = styled.a`
+  color: var(--highlight);
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const PostCover = ({ fluid, link, name }) => (
+  <Figure>
+    <Img fluid={fluid} />
+    <Caption>
+      <Text>Foto de </Text>
+      <Link href={link}>{name}</Link>
+    </Caption>
+  </Figure>
+);
+
+PostCover.propTypes = {
+  fluid: t.object.isRequired,
+  link: t.string.isRequired,
+  name: t.string.isRequired
+};
+
+export default PostCover;
