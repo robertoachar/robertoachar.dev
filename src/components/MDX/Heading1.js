29d120d1941b94705cdac6df875e9fe3a02d7beb
Roberto Achar <robertoachar@gmail.com>
2019-09-09 00:30:10 -0300

    refactor: update font size

--- src/components/MDX/Heading1.js
+++ src/components/MDX/Heading1.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Markup = styled.h1`
   color: var(--highlight);
-  font-size: ${({ theme }) => theme.font.size.huge};
+  font-size: ${({ theme }) => theme.font.size.large};
   font-weight: ${({ theme }) => theme.font.weight.medium};
 `;
 
