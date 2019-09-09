3b251862a29dc9bb761bb90f8ffd2c817ce7976b
Roberto Achar <robertoachar@gmail.com>
2019-09-08 22:02:19 -0300

    refactor: update font sizes

--- src/components/Theme/theme/font.js
+++ src/components/Theme/theme/font.js
@@ -1,6 +1,5 @@
 export default {
   family: {
-    sansSerif: 'proxima-nova, sans-serif',
     sans:
       '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
     serif: 'Georgia, Cambria, "Times New Roman", Times, serif',
@@ -9,9 +8,9 @@ export default {
   size: {
     small: '.875rem',
     normal: '1rem',
-    medium: '1.25rem',
+    medium: '1.5rem',
     large: '2rem',
-    huge: '3rem'
+    huge: '3.5rem'
   },
   weight: {
     thin: 200,
