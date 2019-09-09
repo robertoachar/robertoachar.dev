70ce70a401d8a6a78780318e1cffa902d986c4ee
Roberto Achar <robertoachar@gmail.com>
2019-09-09 00:30:36 -0300

    refactor: increase base font size

--- src/components/Theme/GlobalStyle.js
+++ src/components/Theme/GlobalStyle.js
@@ -3,7 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   html {
     box-sizing: border-box;
-    font-size: 16px;
+    font-size: 20px;
     scroll-behavior: smooth;
   }
 
