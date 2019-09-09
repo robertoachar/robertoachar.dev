604c9ac29eba2b93a66fdaf3f44d7d06d65c171e
Roberto Achar <robertoachar@gmail.com>
2019-09-08 22:24:49 -0300

    refactor: update margin

--- src/components/MDX/Paragraph.js
+++ src/components/MDX/Paragraph.js
@@ -3,7 +3,10 @@ import styled from 'styled-components';
 const Markup = styled.p`
   font-size: ${({ theme }) => theme.font.size.normal};
   line-height: 1.5;
-  margin-top: 1rem;
+
+  &:not(:first-child) {
+    margin-top: 1rem;
+  }
 `;
 
 export default Markup;
