5d128eccefbe74df916bc03bab383be530af43e6
Roberto Achar <robertoachar@gmail.com>
2019-09-09 13:06:17 -0300

    refactor: add margin

--- src/components/MDX/Blockquote.js
+++ src/components/MDX/Blockquote.js
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 const Markup = styled.blockquote`
   border-left: 4px solid var(--highlight);
   color: var(--secondary);
-  padding-left: 1.5rem;
   font-style: italic;
+  padding-left: 1.5rem;
+  margin-top: 1rem;
 `;
 
 export default Markup;
