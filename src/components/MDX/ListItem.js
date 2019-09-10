96c0b0fd693f9eff187c4d7b93a08b682c0e091e
Roberto Achar <robertoachar@gmail.com>
2019-09-09 23:39:53 -0300

    refactor: update list item

--- src/components/MDX/ListItem.js
+++ src/components/MDX/ListItem.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 const Markup = styled.li`
+  line-height: 1.6;
+  word-break: break-word;
+
   &:not(:first-child) {
     margin-top: ${({ theme }) => theme.space.normal};
   }
