f7b4c70479ffa2b29a77f533cabc5c0ae20f102d
Roberto Achar <robertoachar@gmail.com>
2019-09-26 18:27:56 -0300

    fix: add word-break

--- src/components/MDX/Paragraph.js
+++ src/components/MDX/Paragraph.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const Markup = styled.p`
   font-size: ${({ theme }) => theme.font.size.normal};
   line-height: 1.6;
+  word-break: break-word;
 
   &:not(:first-child) {
     margin-top: ${({ theme }) => theme.space.normal};
